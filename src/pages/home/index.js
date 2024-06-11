aaef4c332036d2e8577238c56499f6fe96ca8498
lucasdsra <lucas.dsra@gmail.com>
2024-06-11 19:02:22 -0300

    criação de alguns itens

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,31 +1,44 @@
+import { useState } from "react";
 import {Header} from "../../components/Header";
 import backgroud from "../../assets/background.svg";
+import {Input} from "../../components/Input"
+import ItemList from "../../components/itemList";
 import "./styles.css";
 
 function App() {
-  return (
-    <div className="App">
-      <Header></Header>
-      <div className="conteudo">
-        <img src={backgroud} className="background" alt="background app GitField"></img>
-        <div className="informacoes">
-          <div>
-            <input name="usuario" placeholder="@username"></input>
-            <button>Buscar</button>
-          </div>
-          <div className="imgPerfil">
-            <img src="https://avatars.githubusercontent.com/u/119496831?s=400&u=265c3e868a06ef21c13c521385134face68430d5&v=4" className="profile" alt="imagem de perfil"></img>
-          </div>
-          <div>
-            <h3>Lucas Ramalho Saraiva Dantas</h3>
-            <p>Descrição</p>
-          </div>
-      
-        </div>
-      </div>
+	const [user, setUser] = useState('')
 
-    </div>
-  );
+	return (
+		<div className="App">
+			<Header></Header>
+			<div className="conteudo">
+				<img src={backgroud} className="background" alt="background app GitField"></img>
+				<div className="informacoes">
+					<div>
+						<Input nama="usuario" value={user} onChange={event => setUser(event.target.value)}></Input>
+					</div>
+					<div className="perfil">
+						<img src="https://avatars.githubusercontent.com/u/119496831?s=400&u=265c3e868a06ef21c13c521385134face68430d5&v=4" className="profile" alt="imagem de perfil">
+						</img>
+						<div className="inforUsuario">
+							<h3>Lucas Ramalho Saraiva Dantas</h3>
+							<span>@lucasdsra</span>
+							<p>Descrição</p>
+						</div>
+					</div>
+					<hr></hr>
+					<div className="repositorios">
+						<h4>Repositórios</h4>
+						<ItemList title="teste" description="teste"></ItemList>
+
+					</div>
+
+				</div>
+				
+			</div>
+
+		</div>
+	);
 }
 
 export default App;
